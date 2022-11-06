12e47b48163255d6870091555a17c8784675ae64
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-11-06 18:26:19 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-textshadow'),
+    require('tailwind-scrollbar-hide'),
+    require('tailwind-scrollbar'),
+  ],
 }
