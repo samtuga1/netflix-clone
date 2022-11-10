e92f9ca9c6cf8ff204f8ad5390703a38275d50c4
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-11-10 11:56:46 +0000

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,25 +2,28 @@ import { SearchIcon } from "@heroicons/react/outline";
 import { BellIcon } from "@heroicons/react/outline";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const { logout } = useAuth();
+
   useEffect(() => {
     const handleScroll = () => {
-      if(window.scrollY > 0) {
+      if (window.scrollY > 0) {
         setIsScrolled(true);
-      }else {
+      } else {
         setIsScrolled(false);
       }
-    }
-    window.addEventListener('scroll', handleScroll);
+    };
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-    }
-  }, [])
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <header className={`${isScrolled && 'bg-[#141414]'}`} >
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
       {/* Left side of nav bar */}
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
@@ -30,26 +33,27 @@ function Header() {
           className="cursor-pointer object-contain"
         />
         <ul className="hidden md:flex space-x-4">
-          <li className="headerLink" >Home</li>
-          <li className="headerLink" >Tv Shows</li>
-          <li className="headerLink" >Movies</li>
-          <li className="headerLink" >New & Popular</li>
-          <li className="headerLink" >My list</li>
+          <li className="headerLink">Home</li>
+          <li className="headerLink">Tv Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My list</li>
         </ul>
       </div>
 
       {/* Right side of navbar */}
-      <div className="font-light text-sm flex space-x-4" >
+      <div className="font-light text-sm flex space-x-4">
         <SearchIcon className="hidden sm:inline h-6 w-6" />
-        <p className="hidden lg:inline" >Kids</p>
-        <BellIcon className="h-6 w-6"/>
-        <Link href='/account'>
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        {/* <Link href='/account'> */}
         <img
-            src="https://rb.gy/g1pwyx"
-            alt=""
-            className="cursor-pointer rounded"
-          />
-        </Link>
+          onClick={logout}
+          src="https://rb.gy/g1pwyx"
+          alt=""
+          className="cursor-pointer rounded"
+        />
+        {/* </Link> */}
       </div>
     </header>
   );
