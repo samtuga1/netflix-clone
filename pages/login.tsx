e36e867834dff5c0c1b1a9b642352a6d98e74bbc
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-11-10 11:56:59 +0000

    update

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import useAuth from "../hooks/useAuth";
 
 interface Inputs {
   email: string;
@@ -11,6 +12,7 @@ interface Inputs {
 
 function Login() {
   const [login, setLogin] = useState(false);
+  const {signIn, signUp} = useAuth()
 
   const {
     register,
@@ -19,10 +21,10 @@ function Login() {
   } = useForm<Inputs>();
   const onSubmit = handleSubmit( async ({email, password}) => {
     if(login) {
-      // await signIn(email, password);
+      await signIn(email, password);
     }
     else {
-      // await signUp(email, password);
+      await signUp(email, password);
     }
   });
 
