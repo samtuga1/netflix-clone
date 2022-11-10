db54725c2849ea09e1d2eb5506e338b342103448
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-11-10 19:17:50 +0000

    update

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,4 +1,9 @@
-import { PlusIcon, XIcon } from "@heroicons/react/solid";
+import {
+  PlusIcon,
+  VolumeOffIcon,
+  VolumeUpIcon,
+  XIcon,
+} from "@heroicons/react/solid";
 import MuiModal from "@mui/material/Modal";
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
@@ -6,6 +11,7 @@ import { modalState, movieState } from "../atoms/modalAtom";
 import { Element, Genre } from "../typings";
 import ReactPlayer from "react-player/lazy";
 import { FaPlay } from "react-icons/fa";
+import { ThumbUpIcon } from "@heroicons/react/outline";
 
 function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState);
@@ -44,7 +50,7 @@ function Modal() {
 
   return (
     <MuiModal
-      className="fixed rounded-md !top-7 left-0 right-0 z-50 mx-auto max-w-5xl w-full overflow-hidden overflow-y-scroll scrollbar-hide"
+      className="fixed rounded-md !top-7 left-0 right-0 z-50 mx-auto max-w-3xl w-full overflow-hidden overflow-y-scroll scrollbar-hide"
       open={showModal}
       onClose={handleClose}
     >
@@ -65,15 +71,66 @@ function Modal() {
             muted={muted}
           />
           <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
-            <div>
-              <button className="flex items-center gap-x-2 rounded px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
+            <div className="flex space-x-2">
+              <button className="flex items-center gap-x-2 rounded px-8 text-xl font-bold text-black bg-white transition hover:bg-[#e6e6e6]">
                 <FaPlay className="h-7 w-7 text-black" />
                 Play
               </button>
+              <button className="modalButton">
+                <PlusIcon className="h-7 w-7" />
+              </button>
+              <button className="modalButton">
+                <ThumbUpIcon className="h-7 w-7" />
+              </button>
             </div>
-            <button className="modalButton">
-                <PlusIcon />
+            <button
+              className="modalButton"
+              onClick={() => {
+                setMuted(!muted);
+              }}
+            >
+              {muted ? (
+                <VolumeOffIcon className="h-6 w-6" />
+              ) : (
+                <VolumeUpIcon className="h-6 w-6" />
+              )}
             </button>
+          </div>
+        </div>
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8" >
+          <div className="text-lg space-y-6" >
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie!.vote_average * 10}% Match
+              </p>
+              <p className="font-light">
+                {movie?.release_date || movie?.first_air_date}
+              </p>
+              <div className="flex border items-center justify-center h-4 rounded border-white/40 text-xs px-1.5">
+                HD
+              </div>
+            </div>
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row" >
+              <p className="w-5/6">{movie?.overview}</p>
+              <div>
+                <div className="gap-x-2 text-sm space-y-3" >
+                  <span className="text-[grey]" >Genre: </span>
+                  {genre
+                    .map((gen) => {
+                     return gen.name;
+                    })
+                    .join(", ")}
+                </div>
+                <div className="flex gap-x-2 text-sm space-y-3" >
+                  <span className="text-[grey]" >Original Language:</span>
+                  {movie?.original_language}
+                </div>
+                <div className="flex gap-x-2 text-sm space-y-3" >
+                  <span className="text-[grey]" >Total Votes:</span>
+                  {movie?.vote_count}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </>
